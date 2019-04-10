1905dbdc849adac883c335f3f786a2becaaceb05
tbarna1 <47153727+tbarna1@users.noreply.github.com>
2019-04-09 20:06:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ app.get('/', (request, response) => {
 
 app.get('/greeting', (request, response) => {
   // FIXME: If a name is not given, the app says 'Hello undefined'
-  const message = `Hello ${request.query.name}`;
+  //const message = `Hello ${request.query.name}`;
+  const message='';
   response.status(200).send(message);
 });
 
